0e5b838f21cb876d1589fef60bee99b15a1b472b
Emil Olofsson <emil@turborilla.com>
2023-02-16 10:39:07 +0100

    Add rigid bodies to blobb and falling object

--- Assets/Scripts/TempFallingObject.cs
+++ Assets/Scripts/TempFallingObject.cs
@@ -2,17 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TempFallingObject : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class TempFallingObject : MonoBehaviour {
+    public Rigidbody rigidbody;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private Vector3 startPosition;
+
+    private void Awake() {
+        startPosition = transform.position;
     }
 }
