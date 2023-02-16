5e2321347f72d1de23865731a9e2ce58a3ffca0b
Emil Olofsson <emil@turborilla.com>
2023-02-16 11:59:57 +0100

    Implement reset function

--- Assets/Scripts/TempFallingObject.cs
+++ Assets/Scripts/TempFallingObject.cs
@@ -6,8 +6,17 @@ public class TempFallingObject : MonoBehaviour {
     public Rigidbody rigidbody;
 
     private Vector3 startPosition;
+    private Quaternion startRotation;
 
     private void Awake() {
         startPosition = transform.position;
+        startRotation = transform.rotation;
+    }
+
+    public void Reset() {
+        transform.position = startPosition;
+        transform.rotation = startRotation;
+        rigidbody.velocity = Vector3.zero;
+        rigidbody.angularVelocity = Vector3.zero;
     }
 }
