32aef159f40e0e59ab97a8fd3a2c349905dabd3d
Emil Olofsson <emil@turborilla.com>
2023-02-16 11:02:59 +0100

    Listen for collision and calculate momentum

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,17 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class GameManager : MonoBehaviour {
+    public TempFallingObject fallingObject;
+    public BlobbObject blobbObject;
 
-    // Update is called once per frame
-    void Update()
-    {
+    public void OnObjectImpact(Vector3 point, Vector3 responseMomentum) {
         
     }
 }
